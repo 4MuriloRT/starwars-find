8eb1e01fea0362e5dd2471b498c852cd97a22211
4MuriloRT <murilotaborda09@gmail.com>
2026-03-12 22:48:21 -0300

    feat: Adiciona componentes do shadcn, e organização do layout

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,28 +1,51 @@
 import { swapiService } from "@/services/swapi"
 import { NavBar } from "./NavBar"
+import Image from "next/image";
 import { CharacterCard } from "./CharacterCard";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
+import { Search } from "lucide-react";
 
 export const MainContainer = async() => {
     const characters = await swapiService.getCharacters();
     
-    return(
-        <div className="min-h-screen bg-black text-white">
+    return (
+        <div className="min-h-screen bg-black text-white relative overflow-x-hidden">
             <NavBar />
-            
-            <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-                <header className="mb-12">
-                <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl">
-                    Personagens <span className="text-yellow-500">SWAPI</span>
-                </h1>
-                <p className="mt-4 text-zinc-400">Explore os habitantes da galáxia muito, muito distante.</p>
+            <div className="hidden xl:block fixed left-0 bottom-0 z-0 opacity-50 hover:opacity-100 transition-opacity">
+                <Image 
+                    src="/vader.png" 
+                    alt="Vader" 
+                    width={400} 
+                    height={600} 
+                    className="object-contain"
+                />
+            </div>
+            <div className="hidden xl:block fixed right-0 bottom-0 z-0 opacity-50 hover:opacity-100 transition-opacity">
+                <Image 
+                    src="/obi-wan.png" 
+                    alt="Obi-Wan" 
+                    width={400} 
+                    height={600} 
+                    className="object-contain"
+                />
+            </div>
+            <main className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+                <header className="mb-12 text-center">
+                    <div className="flex w-full justify-center">
+                        <InputGroup className="max-w-xs">
+                            <InputGroupInput placeholder="Procurar Personagem" />
+                            <InputGroupAddon>
+                                <Search/>
+                            </InputGroupAddon>
+                        </InputGroup>
+                    </div>
                 </header>
-
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                {characters.map((person) => (
-                    <CharacterCard key={person.name} person={person} />
-                ))}
+                    {characters.map((person) => (
+                        <CharacterCard key={person.name} person={person} />
+                    ))}
                 </div>
             </main>
         </div>
     )
-}   
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,36 @@
+import { Menubar } from "@base-ui/react";
 import Image from "next/image";
+import { MenubarMenu, MenubarTrigger } from "./ui/menubar";
 
 export const NavBar = () => {
     return(
-        <header className="fixed top-0 left-0 right-0 z-40 lg:static">
+        <header className="top-0 left-0 right-0 z-40">
             <div className="flex items-center">
-                <div className="h-10 flex items-center mx-auto px-4 ">
-                    <Image src="/Logo.png" alt="Logo site" width={100} height={100} />
+                <div className="flex items-center mx-auto px-4 ">
+                    <Image 
+                        src="/Logo-Principal.png" 
+                        alt="Logo site" 
+                        width={200} 
+                        height={200} 
+                        className="w-auto h-auto mt-10 object-contain"
+                    />
                 </div>
+            </div>
+            <div className="flex w-full justify-center">
+                <Menubar className="flex w-72 mt-5 justify-center">
+                <MenubarMenu>
+                    <MenubarTrigger>File</MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Edit</MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>View</MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Profiles</MenubarTrigger>
+                </MenubarMenu>
+                </Menubar>
             </div>
         </header>
     );
