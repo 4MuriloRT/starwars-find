b6fd741101eea1efa65e956c3c9fa82de65902b5
4MuriloRT <murilotaborda09@gmail.com>
2026-03-13 18:30:17 -0300

    refactor: faz ajuster para o deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {MainContainer} from "@/components/MainContainer";
 export default function Home({
   searchParams,
 }: {
-  searchParams: Promise<{ q?: string }>;
+  searchParams: Promise<{ q?: string; category?: string }>;
 }) {
   return <MainContainer searchParams={searchParams} />;
 }
